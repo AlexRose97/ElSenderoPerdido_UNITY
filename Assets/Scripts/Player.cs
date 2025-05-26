469a10c8098c126c70bb272b407e49accb4e7afd
AlexRose97 <aexlopez@gmail.com>
2025-05-25 23:11:02 -0600

    feat(): se agrega manejo de animaciones por inputs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,20 +9,50 @@ public class Player : MonoBehaviour
 
     [SerializeField] private float fuerzaSalto;
 
+    private Animator animator;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    // Update is called once per framea
     void Update()
     {
-        inputHorizontal = Input.GetAxis("Horizontal");
-        rb.linearVelocity = new Vector2(inputHorizontal * velocidadMovimiento, rb.linearVelocity.y);
+        Motion();
+        Jump();
+    }
+
+    private void Jump()
+    {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
+            animator.SetTrigger("jump");
+        }
+    }
+
+    private void Motion()
+    {
+        inputHorizontal = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(inputHorizontal * velocidadMovimiento, rb.linearVelocity.y);
+        if (inputHorizontal != 0)
+        {
+            animator.SetBool("running", true);
+            if (inputHorizontal > 0)
+            {
+                transform.eulerAngles = Vector3.zero;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 180, 0);
+            }
+        }
+        else
+        {
+            animator.SetBool("running", false);
         }
     }
 }
